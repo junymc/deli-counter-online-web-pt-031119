989af95a7f6019ec897fd3bbe028cff3968afb5e
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-19 20:55:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 def line(katz_deli)
+  current_line = "The line is currently: "
   if katz_deli !== 0
     katz_deli.each_with_index do |name, index|
-      puts "The line is currently: #{index + 1}. #{name}"
+      puts current_line << "#{index + 1}. #{name}"
   end
   elsif katz_deli.length == 0
   puts "The line is currently empty."
