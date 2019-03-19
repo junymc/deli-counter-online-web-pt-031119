2199bb48914826015daf0c9068414b32f4d7a781
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-19 21:05:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,6 @@ def line(katz_deli)
     katz_deli.each_with_index do |name, index|
       puts current_line << "#{index + 1}. #{name}"
     end
-    puts current_line.join(" ")
+    puts current_line.join(' ')
   end
 end
